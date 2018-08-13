fd0c0902e25dbd951e452b5ab58eed0a5364a563
Kerinlin <18296613547@qq.com>
2018-08-13 17:23:30 +0800

    完成路由按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
 Vue.use(Router)
+// const Home = r => require.ensure([], () => r(require('@/pages/home/Home.vue')), 'Home')
+const Home = resolve => require(['@/pages/home/Home.vue'], resolve);
+const Subscribe = resolve => require(['@/pages/subscribe/Subscribe.vue'], resolve);
+const Merchandise = resolve => require(['@/pages/merchandise/Merchandise.vue'], resolve);
+const Playtime = resolve => require(['@/pages/playtime/Playtime.vue'], resolve);
+const Personal = resolve => require(['@/pages/personal/My.vue'], resolve);
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
     {
       path: '/home',
       name: 'home',
